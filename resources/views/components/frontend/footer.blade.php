9e6f1d238774045a30df50e288d154f242d4dff1
Muhamad Adib Minanurohman <inanadib084@gmail.com>
2024-05-23 07:59:38 +0700

    Initial commit

---
+++ resources/views/components/frontend/footer.blade.php
@@ -0,0 +1,20 @@
+<!-- START: ASIDE MENU -->
+<section class="">
+    <div class="border-t border-b border-gray-200 py-12 mt-16 px-4">
+    <div class="flex justify-center mb-8">
+        <img
+        src="/frontend/images/content/logo2.png"
+        alt="City Furniture | Penuhi rumah Anda dengan furnitur cantik"
+        style="width: 100px; height: 50px"
+      />
+    </div>
+    </div>
+</section>
+
+<!-- START: FOOTER -->
+<footer class="flex text-center px-4 py-8 justify-center">
+    <p class="text-sm">
+    Copyright 2023 • All Rights Reserved City Furniture by M.Adib Minanurohman
+    </p>
+</footer>
+<!-- END: FOOTER -->
---
+++ resources/views/vendor/jetstream/components/application-mark.blade.php
@@ -0,0 +1,5 @@
+<img
+src="/frontend/images/content/logo2.png"
+alt="City Furniture | Penuhi rumah Anda dengan furnitur cantik"
+style="width: 100px; height: 50px"
+/>
